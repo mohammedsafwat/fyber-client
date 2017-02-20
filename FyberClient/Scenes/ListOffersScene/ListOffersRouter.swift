10cf12ef845ad88b865f6a0ff794cb4b71ac2ec7
Mohammed Safwat <mohammed.safwat@tagstone.com>
2017-02-20 16:40:35 +0400

    format: Format ListOffersRouter class

--- FyberClient/Scenes/ListOffersScene/ListOffersRouter.swift
+++ FyberClient/Scenes/ListOffersScene/ListOffersRouter.swift
@@ -11,52 +11,47 @@
 
 import UIKit
 
-protocol ListOffersRouterInput
-{
-  func navigateToSomewhere()
+protocol ListOffersRouterInput {
+    func navigateToSomewhere()
 }
 
-class ListOffersRouter: ListOffersRouterInput
-{
-  weak var viewController: ListOffersViewController!
+class ListOffersRouter: ListOffersRouterInput {
+    weak var viewController: ListOffersViewController!
   
-  // MARK: - Navigation
-  
-  func navigateToSomewhere()
-  {
-    // NOTE: Teach the router how to navigate to another scene. Some examples follow:
-    
-    // 1. Trigger a storyboard segue
-    // viewController.performSegueWithIdentifier("ShowSomewhereScene", sender: nil)
-    
-    // 2. Present another view controller programmatically
-    // viewController.presentViewController(someWhereViewController, animated: true, completion: nil)
-    
-    // 3. Ask the navigation controller to push another view controller onto the stack
-    // viewController.navigationController?.pushViewController(someWhereViewController, animated: true)
-    
-    // 4. Present a view controller from a different storyboard
-    // let storyboard = UIStoryboard(name: "OtherThanMain", bundle: nil)
-    // let someWhereViewController = storyboard.instantiateInitialViewController() as! SomeWhereViewController
-    // viewController.navigationController?.pushViewController(someWhereViewController, animated: true)
-  }
-  
-  // MARK: - Communication
-  
-  func passDataToNextScene(segue: UIStoryboardSegue)
-  {
-    // NOTE: Teach the router which scenes it can communicate with
-    
-    if segue.identifier == "ShowSomewhereScene" {
-      passDataToSomewhereScene(segue: segue)
+    // MARK: - Navigation
+
+    func navigateToSomewhere() {
+        // NOTE: Teach the router how to navigate to another scene. Some examples follow:
+
+        // 1. Trigger a storyboard segue
+        // viewController.performSegueWithIdentifier("ShowSomewhereScene", sender: nil)
+
+        // 2. Present another view controller programmatically
+        // viewController.presentViewController(someWhereViewController, animated: true, completion: nil)
+
+        // 3. Ask the navigation controller to push another view controller onto the stack
+        // viewController.navigationController?.pushViewController(someWhereViewController, animated: true)
+
+        // 4. Present a view controller from a different storyboard
+        // let storyboard = UIStoryboard(name: "OtherThanMain", bundle: nil)
+        // let someWhereViewController = storyboard.instantiateInitialViewController() as! SomeWhereViewController
+        // viewController.navigationController?.pushViewController(someWhereViewController, animated: true)
     }
-  }
-  
-  func passDataToSomewhereScene(segue: UIStoryboardSegue)
-  {
-    // NOTE: Teach the router how to pass data to the next scene
+
+    // MARK: - Communication
+
+    func passDataToNextScene(segue: UIStoryboardSegue) {
+        // NOTE: Teach the router which scenes it can communicate with
     
-    // let someWhereViewController = segue.destinationViewController as! SomeWhereViewController
-    // someWhereViewController.output.name = viewController.output.name
-  }
+        if segue.identifier == "ShowSomewhereScene" {
+            passDataToSomewhereScene(segue: segue)
+        }
+    }
+
+    func passDataToSomewhereScene(segue: UIStoryboardSegue) {
+        // NOTE: Teach the router how to pass data to the next scene
+
+        // let someWhereViewController = segue.destinationViewController as! SomeWhereViewController
+        // someWhereViewController.output.name = viewController.output.name
+    }
 }
